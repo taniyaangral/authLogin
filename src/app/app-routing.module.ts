6bb133850c5a5c214963e0c8477b1fea28532011
taniyaangral <taniyaangral364@gmail.com>
2021-10-26 18:08:24 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {ForgetPasswordComponent} from './components/forget-password/forget-password.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import { AuthGuard } from './gaurd/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent },
+  {path: 'forget-password', component: ForgetPasswordComponent },
+  {path: '', redirectTo: '/login', pathMatch:'full'},
+  {path: 'admin',
+  canActivate: [AuthGuard],
+   loadChildren: () => import('./modules/admin/admin.module').then((m) => m.AdminModule)},
+  {path: '**', component: NotFoundComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgetPasswordComponent,
+    NotFoundComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
